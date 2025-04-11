069b39752516c59ad92cc790bdb74e12a45592b4
Ethan K <eak2236@columbia.edu>
2025-04-11 06:39:55 -0400

    refactoring Keypoint class

--- DIS2025-04/Assets/Scripts/FrameData.cs
+++ DIS2025-04/Assets/Scripts/FrameData.cs
@@ -4,37 +4,8 @@ using System.Collections.Generic;
 
 public class Keypoint
 {
-    public double x;
-    public double y;
-    public string name;
+    public double x {  get; set; }
+    public double y { get; set; }
+    public string name { get; set; }
 
-    public void SetX(double value)
-    {
-        x = value;
-    }
-
-    public void SetY(double value)
-    {
-        y = value;
-    }
-
-    public void SetName(string value)
-    {
-        name = value;
-    }
-
-    public double GetX()
-    {
-        return x;
-    }
-
-    public double GetY()
-    {
-        return y;
-    }
-
-    public string GetName()
-    {
-        return name;
-    }
 }
