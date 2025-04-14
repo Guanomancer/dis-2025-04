b34747f8225bad9e79077e636d293d3d15f64854
Ethan K <eak2236@columbia.edu>
2025-04-14 10:43:04 -0400

    added conversion support for point3d

--- DIS2025-04/Assets/Scripts/Point3D.cs
+++ DIS2025-04/Assets/Scripts/Point3D.cs
@@ -1,4 +1,5 @@
 using System;
+using UnityEngine;
 
 public class Point3D : Keypoint
 {
@@ -16,6 +17,34 @@ public class Point3D : Keypoint
         X3D = x3D;
         Y3D = y3D;
         Z3D = z3D;
+    }
+
+    public Point3D(string name, Vector2 vec2, Vector3 vec3)
+    {
+        Name = name;
+        X = vec2.x;
+        Y = vec2.y;
+        X3D = vec3.x;
+        Y3D = vec3.y;
+        Z3D = vec3.z;
+    }
+
+    public new Vector2 ToVector2()
+    {
+        return new Vector2((float)X, (float)Y);
+    }
+
+    public Vector3 ToVector3()
+    {
+        return new Vector3((float)X3D, (float)Y3D, (float)Z3D);
+    }
+
+    public static implicit operator Vector2(Point3D p) => new Vector2((float)p.X, (float)p.Y);
+    public static implicit operator Vector3(Point3D p) => new Vector3((float)p.X3D, (float)p.Y3D, (float)p.Z3D);
+
+    public static Point3D FromVectors(string name, Vector2 vec2, Vector3 vec3)
+    {
+        return new Point3D(name, vec2, vec3);
     }
 
     public override string ToString()
