e724351eeaf566cd01784b0ae0f2710eff6f6678
Sarah Than <sthan@colgate.edu>
2025-04-27 16:34:29 +0200

    finished action menu, need to add instructions for each action

--- DIS2025-04/Assets/Scripts/ChangeScenes.cs
+++ DIS2025-04/Assets/Scripts/ChangeScenes.cs
@@ -7,6 +7,26 @@ public class ChangeScenes : MonoBehaviour
         SceneManager.LoadScene("Delete");
     }
 
+    public void GoToRotateScene()
+    {
+        SceneManager.LoadScene("Rotate");
+    }
+
+    public void GoToEditScene()
+    {
+        SceneManager.LoadScene("Edit");
+    }
+
+    public void GoToScaleScene()
+    {
+        SceneManager.LoadScene("Scale");
+    }
+
+    public void GoToMoveScene()
+    {
+        SceneManager.LoadScene("Move");
+    }
+
     public void GoToActionMenu()
     {
         SceneManager.LoadScene("ActionMenu");
