15cf7381592e44e7526a2176ee11615dade92b85
Sarah Than <sthan@colgate.edu>
2025-04-30 22:21:08 +0200

    added radial menu, still need to polish

---
+++ DIS2025-04/Assets/Scripts/KeepUpright.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+public class KeepUpright : MonoBehaviour
+{
+    void LateUpdate()
+    {
+        transform.rotation = Quaternion.LookRotation(UnityEngine.Vector3.forward, UnityEngine.Vector3.up);
+    }
+}
+
