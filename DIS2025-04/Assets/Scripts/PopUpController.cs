c13787da3b6a69cfbbb8b9f653f27ee8c7a69331
Sarah Than <sthan@colgate.edu>
2025-05-03 08:33:28 +0200

    edited action menu directions to match animations in popup

--- DIS2025-04/Assets/Scripts/PopUpController.cs
+++ DIS2025-04/Assets/Scripts/PopUpController.cs
@@ -1,23 +1,36 @@
 using UnityEngine;
 
-public class PopUpController: MonoBehaviour
+public class PopUpController : MonoBehaviour
 {
     public GameObject cheatSheetPanel;
+    public GameObject deleteHand;
+    public GameObject rotateHand;
+    public GameObject scaleHand;
+    public GameObject selectHand;
+    public GameObject moveHand;
 
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.RightShift))
         {
-            cheatSheetPanel.SetActive(!cheatSheetPanel.activeSelf);
+            bool newState = !cheatSheetPanel.activeSelf;
+            cheatSheetPanel.SetActive(newState);
+            //turning hands off when pop up is off
+            deleteHand.SetActive(newState);
+            rotateHand.SetActive(newState);
+            scaleHand.SetActive(newState);
+            selectHand.SetActive(newState);
+            moveHand.SetActive(newState);
         }
 
-        //another way to activate the cheat sheet
+        // another way to activate the cheat sheet
         /*
         if (detectedGesture == "Help")
         {
             cheatSheetPanel.SetActive(true);
+            debugDots.SetActive(true);
         }
         */
-
     }
 }
+
